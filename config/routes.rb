bd9fe465cf450c7ac02b20bbc7c94010b6f91d93
VenkatramanC <venkatraman.c9@gmail.com>
2016-01-11 15:05:00 +0530

    User registrations controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users , :controllers => { registrations: 'registrations' }
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+
    root 'home#land'
 
   # Example of regular route:
