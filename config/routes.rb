da1be48d4f3d9908798922a5fd2b22a0da3587a9
VenkatramanC <venkatraman.c9@gmail.com>
2016-01-11 13:38:28 +0530

    Home land added as a ome page with nav-bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/land'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'home#index'
+   root 'home#land'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
